c2d99182e6feab6977af529b07a577db29a254bb
Neeschal1 <neeschalpok04@gmail.com>
2025-12-13 12:04:19 +0545

    modified user account signup field by adding profile picture field

--- Homedine_Backend/apps/userverification/models.py
+++ Homedine_Backend/apps/userverification/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class UserAccountSignup(models.Model):
+    Users_Profile_Picture = CloudinaryField('image', blank = True, null = True)
     First_Name = models.CharField(max_length=25)
     Last_Name = models.CharField(max_length=25)
     Email = models.EmailField()
