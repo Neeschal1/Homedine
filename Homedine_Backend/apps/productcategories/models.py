6707395c24ce7f2b8afd4eb0a6c5636e57d50380
Neeschal1 <neeschalpok04@gmail.com>
2025-12-15 18:59:14 +0545

    registration of al models for admin

--- Homedine_Backend/apps/productcategories/models.py
+++ Homedine_Backend/apps/productcategories/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class ProductCategories(models.Model):
+    CATEGORY_CHOICES = [
+        ('CUPECO', 'Eco Cup Set'),
+        ('ECOSPOONERY', 'Eco Spoon & Fork Set'),
+        ('BAMBOOPLATE', 'Bamboo Plate Set'),
+        ('WOODBOWL', 'Wooden Bowl Set'),
+        ('LEAFPLATE', 'Leaf Plate Set'),
+        ('COCONUTBOWL', 'Coconut Shell Bowl'),
+        ('CLAYMUG', 'Clay Mug'),
+        ('CLAYPLATE', 'Clay Plate'),
+        ('WOODTRAY', 'Wooden Serving Tray'),
+        ('BAMBOOSTRAW', 'Bamboo Straw Set'),
+        ('STEELSTRAW', 'Steel Straw Set'),
+        ('ECOWATERBOTTLE', 'Eco Water Bottle'),
+        ('WOODCUTLERY', 'Wooden Cutlery Set'),
+        ('BAMBOOCHOPSTICK', 'Bamboo Chopsticks'),
+        ('KITCHENORGANIZER', 'Kitchen Organizer'),
+        ('SPICEBOX', 'Wooden Spice Box'),
+        ('LUNCHBOXECO', 'Eco Lunch Box'),
+        ('TEASETCLAY', 'Clay Tea Set'),
+        ('SERVINGSET', 'Eco Serving Set'),
+    ]
+    
+    Category_Name = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    Category_Picture = CloudinaryField('image')
+    
+    def __str__(self):
+        return self.Category_Name
+    
